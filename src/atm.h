c2b712b3adcc44712dd49f745eda260f3a284b4c
Matt-Y <45631117+Matt-Y-26@users.noreply.github.com>
2021-05-31 00:17:15 -0400

    Update atm.h

--- src/atm.h
+++ src/atm.h
@@ -1,7 +1,10 @@
 #ifndef ATM_H_INCLUDED
 #define CALCULATOR_H_INCLUDED
 #include <iostream>
+
 void login();
 bool validate(std::string ID, std::string pass);
+int read (std::string id, std::string password);
+void options(std::string id);
 
 #endif
