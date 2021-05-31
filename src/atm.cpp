296422ef1e8513918e904fc8a71151b262d8d564
Matt-Y <45631117+Matt-Y-26@users.noreply.github.com>
2021-05-30 20:08:31 -0400

    updated the main
    we could separate the functions to separate files, however since this is really just to get familiar with github again and work a little with c++, it might not be necessary

--- src/atm.cpp
+++ src/atm.cpp
@@ -6,13 +6,15 @@ int main(int agrc, char *agrv[])
 {
         std::string input = "";
         //Call loop func until exit
+        //
+        //init ids and passwds?
         while(1)
         {
         //switch statement might be better, but for now just ifelse
         std::cout<<"Please type 'login' or 'exit'"<<std::endl;
         std::getline (std::cin,input);
         if(input=="login"){
-
+                login();
         }
         else if(input=="exit"){
                 return 0;
@@ -25,5 +27,27 @@ int main(int agrc, char *agrv[])
 }
 
 void login(){
+        //get id and pass
+        std::string ID, pass;
+        std::cout<<"Please input your ID"<<std::endl;
+        std::getline (std::cin,ID);
+        std::cout<<"Please input your your password"<<std::endl;
+        std::getline (std::cin,pass);
 
+        if(validate(ID,pass))
+        {
+
+        }
+        else
+        {
+                std::cout<<"Error. Incorrect Password. Signing out"<<std::endl;
+        }
+        //remove this later
+        exit(0);
+}
+
+bool validate(std::string ID,std::string pass)
+{
+        //obviously, change this later
+        return true;
 }
